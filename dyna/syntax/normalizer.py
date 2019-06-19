b543abbd6b56d847a5c5eacca8cd67af54c6275d
Tim Vieira <tim.f.vieira@gmail.com>
2019-06-19 10:20:06 -0400

    reaping the benefits of mfl's latest changes!

--- dyna/syntax/normalizer.py
+++ dyna/syntax/normalizer.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from dyna.syntax.generic import Term, FVar, Rule
 from dyna.syntax.aggregators import AGG
 from dyna.syntax.syntax import term, run_parser
-from dyna.syntax.util import colors
+from dyna.syntax.util import colors, fib_check
 
 from dyna.interpreter import VariableId, constant, ConstantVariable, intersect
 from dyna.context import dyna_system
@@ -154,7 +154,6 @@ def add_rule(x):
 
     assert iret in set(body.all_vars())
 
-    #from dyna.optimize import run_optimizer
     if DEBUG: print(colors.green % 'rule', rule)
     #rule = run_optimizer(rule, (*args, interpreter.ret_variable))
     #if DEBUG: print(colors.light.yellow % 'optimized rule', rule)
@@ -227,26 +226,6 @@ def test_fib():
 #        #if DEBUG: print(frame)
 #        return interpreter.ret_variable.getValue(frame)
 
-    f = {
-        0: 1,
-        1: 1,
-        2: 2,
-        3: 3,
-        4: 5,
-        5: 8,
-        6: 13,
-        7: 21,
-        8: 34,
-        9: 55,
-        10: 89,
-    }
-#    for N in range(0, 11):
-#        print(f'fib({N})')
-#        got = run_fib(N)         # TODO: use new user_query method.
-#        print('  =', got)
-#        assert got == f[N], f'`fib({N})` got `{got}`, expected `{f[N]}`.'
-#        print()
-
     user_query('fib(5)')
     user_query('fib(N) for range(N,0,11)')
 #    user_query('fib(N)')
@@ -258,33 +237,7 @@ def test_fib():
 #    user_query('N^2+1/N for range(N,0,11)')
 
 
-
 def test_mapl_neural_network():
-    ret_variable = interpreter.ret_variable
-#    add_agg = AggregatorOpImpl(lambda a,b: a+b)
-#    eq_agg = AggregatorOpImpl(lambda a,b: 1/0)  # error if there are more than one key
-
-#    ws = [(0,2),
-#         (-1,3),
-#         (1,5)]
-#    weights = Partition((VariableId(0), ret_variable),
-#                        [intersect(Unify(VariableId(0), constant(w[0])),
-#                                   Unify(ret_variable, constant(w[1]))) for w in ws])
-#    dyna_system.define_term('weights', 1, weights)
-
-    # neural_input(&inp(X)) = weights(X).
-#    neural_input = Aggregator(ret_variable, (VariableId(0),), VariableId('RR_inp'), eq_agg,
-#                              intersect(BuildStructure('inp', VariableId(0), (VariableId('X'),)),
-#                                        dyna_system.call_term('weights', 1)(VariableId('X'), ret=VariableId('RR_inp'))))
-#    dyna_system.define_term('neural_input', 1, neural_input)
-
-    # neural_output(X) += neural_edge(X, Y) * neural_input(Y).
-#    neural_output = Aggregator(ret_variable, (VariableId(0),), VariableId('RR_out'), add_agg,
-#                               intersect(dyna_system.call_term('neural_input', 1)(VariableId('Y'), ret=VariableId('Yr')),
-#                                         dyna_system.call_term('neural_edge', 2)(VariableId(0), VariableId('Y'), ret=VariableId('Er')),
-#                                         dyna_system.call_term('*', 2)(VariableId('Er'), VariableId('Yr'), ret=VariableId('RR_out'))))
-#    dyna_system.define_term('neural_output', 1, neural_output)
-
     add_rules("""
     weights(0)  = 2.
     weights(-1) = 3.
@@ -295,30 +248,8 @@ def test_mapl_neural_network():
     neural_edge(&out(X+Z), &inp(X)) = weights(Z).
     """)
 
-    #user_query('weights(I)')
-
-    # TODO: why does the explicit call_term version work but not this?
     user_query('neural_output(&out(0))')
-#    user_query('neural_output(&out(-1))')
-
-    #user_query('neural_edge(U,V)')
-    #user_query('neural_output(I)')
-    #user_query('neural_output(X) for range(X,-1,3)')
-
-    if 1:
-        eo = dyna_system.call_term('neural_output', 1)
-        frame = Frame()
-        vs = {}
-        def cb(R, frame):
-            assert isinstance(R, Terminal)
-            vs[frame[0].arguments[0]] = ret_variable.getValue(frame)
-        eo = saturate(eo, frame)
-        #re,_ = run_optimizer(eo, (VariableId(0), ret_variable))
-        zz = interpreter.make_aggregator_loopable(eo)
-        loop(zz, frame, cb, best_effort=True)
-        print(vs)
-
-        assert vs == {0: 19, -1: 6, 1: 10, -2: 9, 2: 25}
+    user_query('neural_output(X)')
 
 
 def test_simple():
--- dyna/syntax/util.py
+++ dyna/syntax/util.py
@@ -43,3 +43,19 @@ class colors:
     leftarrow = '←'
     rightarrow = '→'
     reset = _reset
+
+
+# Reference for the values of the first few Fibonacci numbers
+fib_check = {
+    0: 1,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 5,
+    5: 8,
+    6: 13,
+    7: 21,
+    8: 34,
+    9: 55,
+    10: 89,
+}
